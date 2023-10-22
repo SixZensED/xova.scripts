d01e74506dc1e1c516394700cacfc3dd99201e35
SixZens <106856251+SixZensED@users.noreply.github.com>
2023-10-22 09:17:01 +0700

    Update Loader.lua

--- Init/Loader.lua
+++ Init/Loader.lua
@@ -1,13 +1,1 @@
-gameid = {
-	[4777817887] = "https://raw.githubusercontent.com/SixZensED/xova-s.scripts/main/Init/ID/4777817887.lua",
-	[4730278139] = "https://raw.githubusercontent.com/SixZensED/xova-s.scripts/main/Init/ID/4730278139.lua",
-	[4908792642] = "https://raw.githubusercontent.com/SixZensED/xova-s.scripts/main/Init/ID/4908792642.lua",
-}
-
-xpcall(function()
-	for _,id in pairs(gameid) do
-		if game.GameId == _ then
-			loadstring(game:HttpGet(id))()
-		end
-	end
-end,print)
+Kuy อ่านหาควยไร
